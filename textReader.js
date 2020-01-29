61b1ca9fb834f0fdb9505ece9624d5d34a794f2b
Graham Billington <Grahamhbillington@gmail.com>
2020-01-29 15:46:07 -0500

    Update textReader.js

--- textReader.js
+++ textReader.js
@@ -1,26 +1,46 @@
 $(document).ready(function () {
+
+    //changes indexes (line selected) with arrow keys
+    document.addEventListener("keyup", function (e) {
+        if (e.keyCode == 38 && index >= 1) {
+            index--;
+            console.log(index);
+        }
+        if (e.keyCode == 40) {
+            index++;
+            console.log(index);
+        }
+    });
+
+    var index = 0;
+
     var p = $('p');
 
     p.each(function () {
         var current = $(this);
+
         var text = current.text();
 
         var words = text.split(' ');
 
         for (var i = 0; i < words.length; i++) {
-            current.append(' <span>' + words[i] + '</span> ');
-        }
-        var wordsInSpan = current.find('span');
 
+            current.append(' <span>' + words[i] + '</span> ');
+
+        }
+        
+        var wordsInSpan = current.find('span');
+        console.log(wordsInSpan);
         var offsetHeights = [];
         //for each word, compare it's offsetTop to the next words offsetTop, and push offset into offsetHeights
         words.forEach(function () {
-            for (var i = 0; i < words.length - 1; i++) {
-                if (wordsInSpan[i].offsetTop == wordsInSpan[i + 1].offsetTop) {
-                    offsetHeights.push(wordsInSpan[i].offsetTop);
-                }
-            }
-            return offsetHeights;
+            offsetHeights.push(this.offsetTop); 
+            // for (var i = 0; i < words.length - 1; i++) {
+            //     if (words[i].offsetTop == words[i + 1].offsetTop) {
+            //         offsetHeights.push(words[i].offsetTop);
+            //     }
+            // }
+            // return offsetHeights;
         });
 
         //removes duplicate offsets from offsetHeights and makes a filtered array (filteredOffsets)
@@ -29,30 +49,11 @@ $(document).ready(function () {
             return index === self.indexOf(elem);
         });
 
-        var index = 0;
-
-        //changes indexes (line selected) with arrow keys
-        document.addEventListener("keyup", function (e) {
-            if (e.keyCode == 38) {
-                index--;
-                console.log(index);
-            }
-            if (e.keyCode == 40) {
-                index++;
-                console.log(index);
-            }
-        })
-
         //code to highlight each line (not working)
-        filteredOffsets.forEach(function () {
-            for (var i = 0; i < filteredOffsets.length; i++) {
-                if (filteredOffsets[i] == filteredOffsets[index]) {
-                    //highlight line here
-                    console.log("selected index: " + filteredOffsets[index]);
-                }
+        words.forEach(function () {
+            if (this.offsetTop == filteredOffsets[index]) {
             }
         });
-
 
         //var words = ["test", "word"]
         //foreach words if(offsetHeights[i] does not contains 340)
