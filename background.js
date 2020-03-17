c3d05e3e454f070dd8fcf26e50a189e9e39d2170
Graham Billington <Grahamhbillington@gmail.com>
2020-03-17 18:46:23 -0400

    Update background.js

--- background.js
+++ background.js
@@ -9,8 +9,15 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.msg == "initiate") {
         if (isInstalled) {
             chrome.runtime.sendMessage({msg: "Extension installed"});
-            console.log("message sent");
             isInstalled = false;
         }
     } 
+})
+
+//listen for tab change
+chrome.tabs.onActivated.addListener(function() {
+    //send message to content script
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {msg: "tab changed"})
+    })
 })
