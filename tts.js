99a0bedd5f277f41bba30ddb7a9d289522804507
Graham Billington <grahamhbillington@gmail.com>
2020-06-05 00:04:57 -0400

    updated tts popus

--- tts.js
+++ tts.js
@@ -4,4 +4,32 @@ $(document).ready(function () {
     $('#backLink').click(function () {
         chrome.storage.sync.set({ currentPage: 'defaultPopup.html' });
     })
+
+    var synth = window.speechSynthesis;
+    var select = $('#select')
+
+    var voices = [];
+
+    function populateVoiceList() {
+        voices = synth.getVoices();
+
+        for (i = 0; i < voices.length; i++) {
+            var option = document.createElement('option');
+            option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+
+            if (voices[i].default) {
+                option.textContent += ' -- DEFAULT';
+            }
+
+            option.setAttribute('data-lang', voices[i].lang);
+            option.setAttribute('data-name', voices[i].name);
+            select.append(option);
+        }
+    }
+
+    populateVoiceList();
+    if (speechSynthesis.onvoiceschanged !== undefined) {
+        speechSynthesis.onvoiceschanged = populateVoiceList;
+    }
+
 })
